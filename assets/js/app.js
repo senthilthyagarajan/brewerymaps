374bc348e44bee918e2ec70117b79fc7f9f45878
Senthil Thyagarajan <senthil.thyagarajan@gmail.com>
2017-06-08 20:23:11 -0500

    Cleaning for first cut.

--- assets/js/app.js
+++ assets/js/app.js
@@ -149,79 +149,7 @@ var highlightStyle = {
   fillOpacity: 0.7,
   radius: 10
 };
-/* Commenting this  Layers */
-/*var boroughs = L.geoJson(null, {
-  style: function (feature) {
-    return {
-      color: "black",
-      fill: false,
-      opacity: 1,
-      clickable: false
-    };
-  },
-  onEachFeature: function (feature, layer) {
-    boroughSearch.push({
-      name: layer.feature.properties.BoroName,
-      source: "Boroughs",
-      id: L.stamp(layer),
-      bounds: layer.getBounds()
-    });
-  }
-});
-$.getJSON("data/boroughs.geojson", function (data) {
-  boroughs.addData(data);
-});
 
-//Create a color dictionary based off of subway route_id
-var subwayColors = {"1":"#ff3135", "2":"#ff3135", "3":"ff3135", "4":"#009b2e",
-    "5":"#009b2e", "6":"#009b2e", "7":"#ce06cb", "A":"#fd9a00", "C":"#fd9a00",
-    "E":"#fd9a00", "SI":"#fd9a00","H":"#fd9a00", "Air":"#ffff00", "B":"#ffff00",
-    "D":"#ffff00", "F":"#ffff00", "M":"#ffff00", "G":"#9ace00", "FS":"#6e6e6e",
-    "GS":"#6e6e6e", "J":"#976900", "Z":"#976900", "L":"#969696", "N":"#ffff00",
-    "Q":"#ffff00", "R":"#ffff00" };
-
-var subwayLines = L.geoJson(null, {
-  style: function (feature) {
-      return {
-        color: subwayColors[feature.properties.route_id],
-        weight: 3,
-        opacity: 1
-      };
-  },
-  onEachFeature: function (feature, layer) {
-    if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Division</th><td>" + feature.properties.Division + "</td></tr>" + "<tr><th>Line</th><td>" + feature.properties.Line + "</td></tr>" + "<table>";
-      layer.on({
-        click: function (e) {
-          $("#feature-title").html(feature.properties.Line);
-          $("#feature-info").html(content);
-          $("#featureModal").modal("show");
-
-        }
-      });
-    }
-    layer.on({
-      mouseover: function (e) {
-        var layer = e.target;
-        layer.setStyle({
-          weight: 3,
-          color: "#00FFFF",
-          opacity: 1
-        });
-        if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-        }
-      },
-      mouseout: function (e) {
-        subwayLines.resetStyle(e.target);
-      }
-    });
-  }
-});
-$.getJSON("data/subways.geojson", function (data) {
-  subwayLines.addData(data);
-});
-*/
 /* Single marker cluster layer to hold all clusters */
 var markerClusters = new L.MarkerClusterGroup({
   spiderfyOnMaxZoom: true,
@@ -233,92 +161,7 @@ var markerClusters = new L.MarkerClusterGroup({
 
 /* Empty layer placeholder to add to layer control for listening when to add/remove theaters to markerClusters layer */
 
-/*
-var theaterLayer = L.geoJson(null);
-var theaters = L.geoJson(null, {
-  pointToLayer: function (feature, latlng) {
-    return L.marker(latlng, {
-      icon: L.icon({
-        iconUrl: "assets/img/theater.png",
-        iconSize: [24, 28],
-        iconAnchor: [12, 28],
-        popupAnchor: [0, -25]
-      }),
-      title: feature.properties.NAME,
-      riseOnHover: true
-    });
-  },
-  onEachFeature: function (feature, layer) {
-    if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Phone</th><td>" + feature.properties.TEL + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.ADDRESS1 + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>";
-      layer.on({
-        click: function (e) {
-          $("#feature-title").html(feature.properties.NAME);
-          $("#feature-info").html(content);
-          $("#featureModal").modal("show");
-          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], highlightStyle));
-        }
-      });
-      $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/theater.png"></td><td class="feature-name">' + layer.feature.properties.NAME + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-      theaterSearch.push({
-        name: layer.feature.properties.NAME,
-        address: layer.feature.properties.ADDRESS1,
-        source: "Theaters",
-        id: L.stamp(layer),
-        lat: layer.feature.geometry.coordinates[1],
-        lng: layer.feature.geometry.coordinates[0]
-      });
-    }
-  }
-});
-$.getJSON("data/DOITT_THEATER_01_13SEPT2010.geojson", function (data) {
-  theaters.addData(data);
-  map.addLayer(theaterLayer);
-});*/
 
-/* Empty layer placeholder to add to layer control for listening when to add/remove museums to markerClusters layer */
-/*
-var museumLayer = L.geoJson(null);
-var museums = L.geoJson(null, {
-  pointToLayer: function (feature, latlng) {
-    return L.marker(latlng, {
-      icon: L.icon({
-        iconUrl: "assets/img/museum.png",
-        iconSize: [24, 28],
-        iconAnchor: [12, 28],
-        popupAnchor: [0, -25]
-      }),
-      title: feature.properties.NAME,
-      riseOnHover: true
-    });
-  },
-  onEachFeature: function (feature, layer) {
-    if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Phone</th><td>" + feature.properties.TEL + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.ADRESS1 + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>";
-      layer.on({
-        click: function (e) {
-          $("#feature-title").html(feature.properties.NAME);
-          $("#feature-info").html(content);
-          $("#featureModal").modal("show");
-          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], highlightStyle));
-        }
-      });
-      $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/museum.png"></td><td class="feature-name">' + layer.feature.properties.NAME + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-      museumSearch.push({
-        name: layer.feature.properties.NAME,
-        address: layer.feature.properties.ADRESS1,
-        source: "Museums",
-        id: L.stamp(layer),
-        lat: layer.feature.geometry.coordinates[1],
-        lng: layer.feature.geometry.coordinates[0]
-      });
-    }
-  }
-});
-$.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
-  museums.addData(data);
-});
-*/
 /* Empty layer placeholder to add to layer control for listening when to add/remove breweries to markerClusters layer */
 var breweryLayer = L.geoJson(null);
 var breweries = L.geoJson(null, {
@@ -371,14 +214,7 @@ map = L.map("map", {
 
 /* Layer control listeners that allow for a single markerClusters layer */
 map.on("overlayadd", function(e) {
-  // if (e.layer === theaterLayer) {
-  //   markerClusters.addLayer(theaters);
-  //   syncSidebar();
-  // }
-  // if (e.layer === museumLayer) {
-  //   markerClusters.addLayer(museums);
-  //   syncSidebar();
-  // }
+
   if (e.layer === breweryLayer) {
     markerClusters.addLayer(breweries);
     syncSidebar();
@@ -386,14 +222,7 @@ map.on("overlayadd", function(e) {
 });
 
 map.on("overlayremove", function(e) {
-  // if (e.layer === theaterLayer) {
-  //   markerClusters.removeLayer(theaters);
-  //   syncSidebar();
-  // }
-  // if (e.layer === museumLayer) {
-  //   markerClusters.removeLayer(museums);
-  //   syncSidebar();
-  // }
+
   if (e.layer === breweryLayer) {
     markerClusters.removeLayer(breweries);
     syncSidebar();
@@ -480,14 +309,10 @@ var baseLayers = {
 
 var groupedOverlays = {
   "Layers": {
-    // "<img src='assets/img/theater.png' width='24' height='28'>&nbsp;Theaters": theaterLayer,
-    // "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer,
+
     "<img src='assets/img/beer.png' width='24' height='28'>&nbsp;Breweries": breweryLayer
  },
-  // "Reference": {
-  //   "Boroughs": boroughs,
-  //   "Subway Lines": subwayLines
-  // }
+
 };
 
 var layerControl = L.control.groupedLayers(baseLayers, groupedOverlays, {
@@ -519,25 +344,7 @@ $(document).one("ajaxStop", function () {
   featureList = new List("features", {valueNames: ["feature-name"]});
   featureList.sort("feature-name", {order:"asc"});
 
-  // var boroughsBH = new Bloodhound({
-  //   name: "Boroughs",
-  //   datumTokenizer: function (d) {
-  //     return Bloodhound.tokenizers.whitespace(d.name);
-  //   },
-  //   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  //   local: boroughSearch,
-  //   limit: 10
-  // });
-  //
-  // var theatersBH = new Bloodhound({
-  //   name: "Theaters",
-  //   datumTokenizer: function (d) {
-  //     return Bloodhound.tokenizers.whitespace(d.name);
-  //   },
-  //   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  //   local: theaterSearch,
-  //   limit: 10
-  // });
+
 
   var museumsBH = new Bloodhound({
     name: "Museums",
@@ -590,23 +397,7 @@ $(document).one("ajaxStop", function () {
     highlight: true,
     hint: false
   },
-//   {
-  //   name: "Boroughs",
-  //   displayKey: "name",
-  //   source: boroughsBH.ttAdapter(),
-  //   templates: {
-  //     header: "<h4 class='typeahead-header'>Boroughs</h4>"
-  //   }
-  // },
-  // {
-  //   name: "Theaters",
-  //   displayKey: "name",
-  //   source: theatersBH.ttAdapter(),
-  //   templates: {
-  //     header: "<h4 class='typeahead-header'><img src='assets/img/theater.png' width='24' height='28'>&nbsp;Theaters</h4>",
-  //     suggestion: Handlebars.compile(["{{name}}<br>&nbsp;<small>{{address}}</small>"].join(""))
-  //   }
-  // },
+
   {
     name: "Museums",
     displayKey: "name",
